1d71937239bb8e5baea1a465e533532595dd5c34
BennyBoy10 <bulamabenjamin100@gmail.com>
2025-11-10 18:50:31 +0100

    Update QuickSub-Git

--- 1.sh
+++ 1.sh
@@ -6,19 +6,9 @@ echo ""
 echo -e "\e[32mStarting... \e[0m"
 echo ""
 
-git init
 git add .
-git commit -m "Updated QuickSub-Git"
-git branch -M main
-
-# Check if remote exists
-if git remote | grep -q origin; then
-  git remote remove origin  # Remove existing remote
-fi
-
-git remote add origin https://github.com/BennyBoy10/QuickSub.git
-#git push -u origin main
-git push -u origin main --force
+git commit -m "Update QuickSub-Git"
+git push origin main
 
 echo ""
 echo -e "\e[32mDone!! \e[0m"
